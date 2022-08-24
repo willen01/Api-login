69d6d4cbf82849321c835483efeebd8a537b21b5
willen <willen.sesantos@gmail.com>
2022-08-24 18:00:15 -0300

    feat:Cria cadastro de usuário

--- src/domain/controllers/userController.ts
+++ src/domain/controllers/userController.ts
@@ -1,10 +1,17 @@
 import { Request, Response } from "express";
+import UserRepository from "../repositories/UserRepository";
 
 class UserController {
-  register(req: Request, res: Response) {}
-  login(req: Request, res: Response) {
-    res.send("login");
+  async register(req: Request, res: Response) {
+    const createProfile = await UserRepository.register(req.body);
+
+    if (createProfile) {
+      res.status(201).send();
+    } else {
+      res.status(400).send();
+    }
   }
+  login(req: Request, res: Response) {}
 }
 
 export default new UserController();
---
+++ src/domain/repositories/UserRepository.ts
@@ -0,0 +1,21 @@
+import { Profile } from "../entities/profiles";
+import User from "./models";
+
+class UserRepository {
+  async register(dataProfile: Profile) {
+    const profile = new User({
+      name: dataProfile.name,
+      email: dataProfile.email,
+      password: dataProfile.email,
+    });
+
+    try {
+      await profile.save();
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
+}
+
+export default new UserRepository();
