c3aa3411bd2ed293074ce379c9587ed8e55005e7
willen <willen.sesantos@gmail.com>
2022-08-26 12:42:14 -0300

    fix: Altera tamanho max do password
    O tamanho foi alterado para alterado para se adequar ao tamanho da string criptografada gerada

--- src/domain/repositories/models.ts
+++ src/domain/repositories/models.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const profileSchema = new mongoose.Schema({
   name: { type: String, required: true, minlength: 3, maxlength: 15 },
   email: { type: String, required: true, minlegth: 10, maxlength: 50 },
-  password: { type: String, required: true, minlegth: 8, maxlength: 15 },
+  password: { type: String, required: true, minlegth: 8, maxlength: 60 },
 });
 
 export default mongoose.model("profile", profileSchema);
