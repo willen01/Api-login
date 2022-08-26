ea78359f031f50aebd055e36e99f1d46e160f4d7
willen <willen.sesantos@gmail.com>
2022-08-26 10:57:31 -0300

    feat: Adciona tamanho min e max no model
    os dados para cadastro e login passam a ter o tamanho minimo e máximo de catacteres

--- src/domain/repositories/models.ts
+++ src/domain/repositories/models.ts
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
 const profileSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
+  name: { type: String, required: true, minlength: 3, maxlength: 15 },
+  email: { type: String, required: true, minlegth: 10, maxlength: 50 },
+  password: { type: String, required: true, minlegth: 8, maxlength: 15 },
 });
 
 export default mongoose.model("profile", profileSchema);
